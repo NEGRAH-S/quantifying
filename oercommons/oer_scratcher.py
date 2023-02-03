f8ae43352106c0c2e0a47d12e2760c507d1cfab3
Claire Wan <clairewn@umich.edu>
2023-02-03 14:21:56 -0500

    oer access token

---
+++ oercommons/oer_scratcher.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+This file is dedicated to obtain a .csv record report for Open Education Resources Commons data.
+"""
+
+ACCESS_TOKEN = "24edf58088875347463ba7fc13f5ffd25c1b5d72"
+

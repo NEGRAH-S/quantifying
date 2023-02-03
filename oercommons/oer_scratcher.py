07d1132aefc49e15d1b16b8cd1cfe4ae8e0bb4f3
Claire Wan <clairewn@umich.edu>
2023-02-03 14:45:11 -0500

    oer commons starter file

--- oercommons/oer_scratcher.py
+++ oercommons/oer_scratcher.py
@@ -3,5 +3,18 @@
 This file is dedicated to obtain a .csv record report for Open Education Resources Commons data.
 """
 
-ACCESS_TOKEN = "24edf58088875347463ba7fc13f5ffd25c1b5d72"
+import requests
 
+ACCESS_TOKEN = "24edf58088875347463ba7fc13f5ffd25c1b5d72"
+ENDPOINT = "https://www.oercommons.org/api/search"
+BASE_URL = ENDPOINT + "?token=" + ACCESS_TOKEN
+
+def test_access():
+    """Tests endpoint by filtering under cc-by license."""
+    print("test")
+
+def main():
+    test_access()
+
+if __name__ == "__main__":
+    main()

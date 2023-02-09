ebf7caae2d145b7dbd6ac4556e6ad1e32e6492ee
Claire Wan <clairewn@umich.edu>
2023-02-09 12:58:10 -0500

    XML response example

--- oercommons/oer_scratcher.py
+++ oercommons/oer_scratcher.py
@@ -4,14 +4,31 @@ This file is dedicated to obtain a .csv record report for Open Education Resourc
 """
 
 import requests
+import xml.etree.ElementTree as ET
 
 ACCESS_TOKEN = "24edf58088875347463ba7fc13f5ffd25c1b5d72"
 ENDPOINT = "https://www.oercommons.org/api/search"
 BASE_URL = ENDPOINT + "?token=" + ACCESS_TOKEN
 
+def works_per_license():
+    licenses = [
+        "CC",
+        "CC-BY",
+        "CC-BY-NC",
+        "CC-BY-NC-ND",
+        "CC-BY-NC-SA",
+        "CC-BY-ND",
+        "CC-BY-SA",
+        "CC-BY-ND" # no longer available?
+    ]
+    
+
 def test_access():
     """Tests endpoint by filtering under cc-by license."""
-    print("test")
+    response = requests.get(BASE_URL + '&f.license=cc-by&batch_size=10')
+    print(response.status_code)
+    with open('data.xml', 'w') as f:
+        f.write(response.text)
 
 def main():
     test_access()

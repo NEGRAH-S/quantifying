5ab1d269bd00ae4bf844e84baabde9f3ecd8cefd
Claire Wan <clairewn@umich.edu>
2023-02-15 00:02:47 -0500

    set up csv

--- oercommons/oer_scratcher.py
+++ oercommons/oer_scratcher.py
@@ -3,9 +3,10 @@
 This file is dedicated to obtain a .csv record report for Open Education Resources Commons data.
 """
 
-import requests
-import re
+import csv
 import xml.etree.ElementTree as ET
+import re
+import requests
 
 import query_secrets
 
@@ -34,6 +35,31 @@ def get_license_total_count(license):
     return count
 
 
+def batch_retrieve(license, batch_start):
+    """Returns ET object with the next 10 results."""
+    response = requests.get(f'{BASE_URL}&f.license={license}&batch_size=50&batch_start={batch_start}')
+    return ET.fromstring(re.sub(r"&\w*;", "", response.text))
+
+
+def record_license_data(license):
+    """Retrieve all data for a license."""
+    total = get_license_total_count(license)
+    # TODO: loop through totals to get every work
+    # for i in range(0, total/10):
+
+
+def record_all_licenses():
+    """Records the data of all license types."""
+    license_list = get_license_list()
+    with open('oer.csv', 'w', newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["license", "education_level", "subject_area",
+            "material_type", "media_format", "languages", "primary_user", 
+            "educational_use", "modification_date"])
+    # for license_type in license_list:
+    #     record_license_data(license_type)
+
+
 def test_access():
     """Tests endpoint by filtering under cc-by license."""
     response = requests.get(f'{BASE_URL}&f.license=cc-by&batch_size=10') 
@@ -57,7 +83,8 @@ def test_xml_parse():
 def main():
     # test_access()
     # test_xml_parse()
-    print(get_license_total_count('cc-by'))
+    # print(get_license_total_count('cc-by'))
+    record_all_licenses()
 
 if __name__ == "__main__":
     main()
